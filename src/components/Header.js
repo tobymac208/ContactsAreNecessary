66a1929a772b672b9b655a331e1d23f1287471e1
nikf <niklasfernandez97@gmail.com>
2022-01-01 22:02:26 -0600

    Adds hyperlink to Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div className="ui fixed menu">
       <div className="ui container center">
-        <h2>Contact Manager</h2>
+	<Link to='/'>
+          <h2>Contact Manager</h2>
+	</Link>
       </div>
     </div>
   );
